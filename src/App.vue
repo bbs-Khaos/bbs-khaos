5942f6eeb53b8720ceac51fbc8c2ef8be483e5b2
chen <ch.10234en@gmail.com>
2021-08-19 09:48:12 +0800

    :heavy_plug_sign: add plugin of element-plus

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
 </template>
 
 <style lang="less">
+@import "~element-plus/lib/theme-chalk/index.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
